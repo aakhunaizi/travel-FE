361f5320cf7b463d9cd3e6288a02079aa729dec7
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-03-09 21:23:46 +0300

    Added routes

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -12,6 +12,7 @@ import { signup } from "../../store/actions/authActions";
 import { useForm, Controller } from "react-hook-form";
 import ReactDatePicker from "react-datepicker";
 import { FormControlLabel, Radio, RadioGroup } from "@material-ui/core";
+import { Helmet } from "react-helmet";
 
 //Styles
 import { StyledForm, StyledValidationText } from "./styles";
@@ -43,132 +44,141 @@ export default function Signup() {
   };
 
   return (
-    <StyledForm className="container">
-      <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <h1>Sign Up</h1>
-        <br />
-        <label className="form-label">Email</label>
-        <input
-          type="text"
-          name="email"
-          className="form-control mb-2"
-          ref={register({
-            required: "Email is required",
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: "Invalid email address",
-            },
-          })}
-        />
-        <StyledValidationText>
-          {errors.email && <span>{errors.email.message}</span>}
-        </StyledValidationText>
-        <br />
-        <label className="form-label">First Name</label>
-        <input
-          type="text"
-          name="firstName"
-          className="form-control mb-2"
-          ref={register({ required: true })}
-        />
-        <StyledValidationText>
-          {errors.firstName && "First Name is required"}
-        </StyledValidationText>
-        <br />
-        <label className="form-label">Last Name</label>
-        <input
-          type="text"
-          name="lastName"
-          className="form-control mb-2"
-          ref={register({ required: true })}
-        />
-        <StyledValidationText>
-          {errors.lastName && "Last Name is required"}
-        </StyledValidationText>
-        <br />
-        <label>Date of Birth</label>
-        <br />
-        <Controller
-          control={control}
-          name="dateOfBirth"
-          render={(props) => (
-            <ReactDatePicker
-              dateFormat="yyyy/MM/dd"
-              className="form-control mb-2"
-              placeholderText="Select date"
-              onChange={(e) => props.onChange(e)}
-              selected={props.value}
-            />
-          )}
-        />
-        <StyledValidationText>
-          {errors.value && "Date of Birth is required"}
-        </StyledValidationText>
-        <label>Gender</label>
-        <Controller
-          as={
-            <RadioGroup aria-label="gender">
-              <FormControlLabel value="M" control={<Radio />} label="Male" />
-              <FormControlLabel value="F" control={<Radio />} label="Female" />
-            </RadioGroup>
-          }
-          name="gender"
-          control={control}
-        />
-        <label className="form-label">Phone Number</label>
-        <input
-          type="text"
-          name="phoneNumber"
-          className="form-control mb-2"
-          ref={register({ required: true })}
-        />
-        <StyledValidationText>
-          {errors.phoneNumber && "Phone Number is required"}
-        </StyledValidationText>
-        <br />
-        <label className="form-label">Username</label>
-        <input
-          type="text"
-          name="username"
-          className="form-control mb-2"
-          ref={register({ required: true })}
-        />
-        <StyledValidationText>
-          {errors.username && "Username is required"}
-        </StyledValidationText>
-        <br />
-        <label className="form-label">Password</label>
-        <input
-          type={password ? "password" : "text"}
-          name="password"
-          className="form-control mb-2"
-          ref={register({
-            required: "Password is required",
-            pattern: {
-              value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
-              message:
-                "The password must contain a minimum of 8 characters, an uppercase letter, a lowercase letter, and a number.",
-            },
-          })}
-        />
-        <StyledValidationText>
-          {errors.password && <span>{errors.password.message}</span>}
-        </StyledValidationText>
-        <div className="mb-3 form-check">
+    <>
+      <Helmet>
+        <title>Sign Up</title>
+      </Helmet>
+      <StyledForm className="container">
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
+          <h1>Sign Up</h1>
+          <br />
+          <label className="form-label">Email</label>
           <input
-            type="checkbox"
-            className="form-check-input"
-            id="exampleCheck1"
-            onClick={() => setPassword(!password)}
+            type="text"
+            name="email"
+            className="form-control mb-2"
+            ref={register({
+              required: "Email is required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Invalid email address",
+              },
+            })}
           />
-          <label className="form-check-label">Show password</label>
-        </div>
-        <input
-          type="submit"
-          className="form-control btn btn-success"
-          value="Sign Up"
-        />
-      </form>
-    </StyledForm>
+          <StyledValidationText>
+            {errors.email && <span>{errors.email.message}</span>}
+          </StyledValidationText>
+          <br />
+          <label className="form-label">First Name</label>
+          <input
+            type="text"
+            name="firstName"
+            className="form-control mb-2"
+            ref={register({ required: true })}
+          />
+          <StyledValidationText>
+            {errors.firstName && "First Name is required"}
+          </StyledValidationText>
+          <br />
+          <label className="form-label">Last Name</label>
+          <input
+            type="text"
+            name="lastName"
+            className="form-control mb-2"
+            ref={register({ required: true })}
+          />
+          <StyledValidationText>
+            {errors.lastName && "Last Name is required"}
+          </StyledValidationText>
+          <br />
+          <label>Date of Birth</label>
+          <br />
+          <Controller
+            control={control}
+            name="dateOfBirth"
+            render={(props) => (
+              <ReactDatePicker
+                dateFormat="yyyy/MM/dd"
+                className="form-control mb-2"
+                placeholderText="Select date"
+                onChange={(e) => props.onChange(e)}
+                selected={props.value}
+              />
+            )}
+          />
+          <StyledValidationText>
+            {errors.value && "Date of Birth is required"}
+          </StyledValidationText>
+          <label>Gender</label>
+          <Controller
+            as={
+              <RadioGroup aria-label="gender">
+                <FormControlLabel value="M" control={<Radio />} label="Male" />
+                <FormControlLabel
+                  value="F"
+                  control={<Radio />}
+                  label="Female"
+                />
+              </RadioGroup>
+            }
+            name="gender"
+            control={control}
+          />
+          <label className="form-label">Phone Number</label>
+          <input
+            type="text"
+            name="phoneNumber"
+            className="form-control mb-2"
+            ref={register({ required: true })}
+          />
+          <StyledValidationText>
+            {errors.phoneNumber && "Phone Number is required"}
+          </StyledValidationText>
+          <br />
+          <label className="form-label">Username</label>
+          <input
+            type="text"
+            name="username"
+            className="form-control mb-2"
+            ref={register({ required: true })}
+          />
+          <StyledValidationText>
+            {errors.username && "Username is required"}
+          </StyledValidationText>
+          <br />
+          <label className="form-label">Password</label>
+          <input
+            type={password ? "password" : "text"}
+            name="password"
+            className="form-control mb-2"
+            ref={register({
+              required: "Password is required",
+              pattern: {
+                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+                message:
+                  "The password must contain a minimum of 8 characters, an uppercase letter, a lowercase letter, and a number.",
+              },
+            })}
+          />
+          <StyledValidationText>
+            {errors.password && <span>{errors.password.message}</span>}
+          </StyledValidationText>
+          <div className="mb-3 form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+              onClick={() => setPassword(!password)}
+            />
+            <label className="form-check-label">Show password</label>
+          </div>
+          <input
+            type="submit"
+            className="form-control btn btn-success"
+            value="Sign Up"
+          />
+        </form>
+      </StyledForm>
+    </>
   );
 }
