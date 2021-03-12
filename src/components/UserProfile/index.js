84e8cb9e09c216b3508577e8b26df00e3fd2d064
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-03-12 13:32:46 +0300

    Added booking info with dummy data

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -1,10 +1,12 @@
 //Components
+import UserProfileBookingInfo from "../UserProfileBookingInfo";
 import UserProfilePersonalInfo from "../UserProfilePersonalInfo";
 
 const UserProfile = () => {
   return (
     <>
       <UserProfilePersonalInfo />
+      <UserProfileBookingInfo />
     </>
   );
 };
