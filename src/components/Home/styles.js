a01fa3d4a2deb2f03382fd105ea7e6283fc418e5
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-03-13 19:44:45 +0300

    Cleaned up home page

---
+++ src/components/Home/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { CardHeader } from "@material-ui/core";
+
+export const StyledSearch = styled.div`
+  width: 40%;
+  margin-top: 15%;
+`;
+
+export const StyledCardHeader = styled(CardHeader)`
+  text-align: center;
+`;
