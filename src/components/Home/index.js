d8ffcbbc828c1a7761c40453d80be2d3c7ccf54b
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-03-17 18:29:40 +0300

    Modified home page styling

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
 //Components
-import { Button, CardActions } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import Select from "react-select";
 import {
   StyledCard,
@@ -100,98 +100,109 @@ const Home = () => {
   };
 
   return (
-    <StyledSearch className="container">
-      <br />
-      <StyledCard variant="outlined">
-        <StyledCardHeader title="Search for your next destination" />
-        <div className="row p-2">
-          <div className="col-md-4">
-            <Select
-              name="flightType"
-              placeholder="Flight Type"
-              value={flight.flightType}
-              options={_flightType}
-              onChange={(flightType) => {
-                setFlight({ ...flight, flightType });
-              }}
-            />
-          </div>
-          <Select
-            className="col-md-4"
-            placeholder="Passengers"
-            value={flight.passengers}
-            isClearable="true"
-            name="passengers"
-            options={_passengers}
-            onChange={(passengers) => setFlight({ ...flight, passengers })}
-          />
-          <Select
-            className="col-md-4"
-            name="seatType"
-            placeholder="Seat Type"
-            value={flight.seatType}
-            options={_seatType}
-            onChange={(seatType) => setFlight({ ...flight, seatType })}
-          />
-        </div>
-        <div className="row mt-2 p-2">
-          <div className="col-md-6">
-            <Select
-              placeholder={<div>From</div>}
-              isSearchable="true"
-              isClearable="true"
-              name="departureAirport"
-              options={airportDepartureList}
-              onChange={(departureAirport) =>
-                setFlight({ ...flight, departureAirport })
-              }
-            />
-          </div>
-          <div className="col-md-6">
-            <Select
-              placeholder="To"
-              isSearchable="true"
-              isClearable="true"
-              isDisabled={flight.departureAirport ? false : true}
-              name="arrivalAirport"
-              options={airportArrivalList}
-              onChange={(arrivalAirport) =>
-                setFlight({ ...flight, arrivalAirport })
-              }
-            />
-          </div>
-        </div>
-        <div className="row mt-2 p-2">
-          <StyledFaPlaneDeparture />
-          <ReactDatePicker
-            className="form-control p-2"
-            placeholderText="Select date"
-            onChange={(departureDate) => {
-              setFlight({ ...flight, departureDate });
-            }}
-            selected={flight.departureDate}
-          />
-          {flight.flightType.value === "roundtrip" && (
-            <>
-              <StyledFaPlaneArrival />
+    <div
+      className="vh-100"
+      style={{
+        backgroundImage: `url("https://i.redd.it/cy1vbp2axhy41.png")`,
+      }}
+    >
+      <StyledSearch className="container">
+        <br />
+        <br />
+        <br />
+        <div style={{ margin: "10% auto" }}>
+          <StyledCard variant="outlined">
+            <StyledCardHeader title="Search for your next destination" />
+            <div className="row p-2">
+              <div className="col-md-4">
+                <Select
+                  name="flightType"
+                  placeholder="Flight Type"
+                  value={flight.flightType}
+                  options={_flightType}
+                  onChange={(flightType) => {
+                    setFlight({ ...flight, flightType });
+                  }}
+                />
+              </div>
+              <Select
+                className="col-md-4"
+                placeholder="Passengers"
+                value={flight.passengers}
+                isClearable="true"
+                name="passengers"
+                options={_passengers}
+                onChange={(passengers) => setFlight({ ...flight, passengers })}
+              />
+              <Select
+                className="col-md-4"
+                name="seatType"
+                placeholder="Seat Type"
+                value={flight.seatType}
+                options={_seatType}
+                onChange={(seatType) => setFlight({ ...flight, seatType })}
+              />
+            </div>
+            <div className="row mt-2 p-2">
+              <div className="col-md-6">
+                <Select
+                  placeholder={<div>From</div>}
+                  isSearchable="true"
+                  isClearable="true"
+                  name="departureAirport"
+                  options={airportDepartureList}
+                  onChange={(departureAirport) =>
+                    setFlight({ ...flight, departureAirport })
+                  }
+                />
+              </div>
+              <div className="col-md-6">
+                <Select
+                  placeholder="To"
+                  isSearchable="true"
+                  isClearable="true"
+                  isDisabled={flight.departureAirport ? false : true}
+                  name="arrivalAirport"
+                  options={airportArrivalList}
+                  onChange={(arrivalAirport) =>
+                    setFlight({ ...flight, arrivalAirport })
+                  }
+                />
+              </div>
+            </div>
+            <div className="row mt-2 p-2">
+              <StyledFaPlaneDeparture />
               <ReactDatePicker
                 className="form-control p-2"
                 placeholderText="Select date"
-                onChange={(arrivalDate) =>
-                  setFlight({ ...flight, arrivalDate })
-                }
-                selected={flight.arrivalDate}
+                onChange={(departureDate) => {
+                  setFlight({ ...flight, departureDate });
+                }}
+                selected={flight.departureDate}
               />
-            </>
-          )}
+              {flight.flightType.value === "roundtrip" && (
+                <>
+                  <StyledFaPlaneArrival />
+                  <ReactDatePicker
+                    className="form-control p-2"
+                    placeholderText="Select date"
+                    onChange={(arrivalDate) =>
+                      setFlight({ ...flight, arrivalDate })
+                    }
+                    selected={flight.arrivalDate}
+                  />
+                </>
+              )}
+            </div>
+            <StyledCardActions>
+              <Button variant="outlined" color="primary" onClick={handleSearch}>
+                Search
+              </Button>
+            </StyledCardActions>
+          </StyledCard>
         </div>
-        <StyledCardActions>
-          <Button variant="outlined" color="primary" onClick={handleSearch}>
-            Search
-          </Button>
-        </StyledCardActions>
-      </StyledCard>
-    </StyledSearch>
+      </StyledSearch>
+    </div>
   );
 };
 
